fbc2b6bac631178f72fd47f9fdce21dfaa21f48d
Gabriel Garavit <garavitgabriel@gmail.com>
2017-01-01 21:49:05 -0600

    The App is production ready

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,30 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
+
+
+  root to:'welcome/index', to:'welcome#index'
+
+  # get'welcome/index', to:'welcome#index'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
